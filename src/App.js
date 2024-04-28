21ec9de882b03282535d2296dcb96ca92ca0130d
Nathan Maynard <ncmaynard03@gmail.com>
2024-04-27 21:36:03 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
+import Library from './Library';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from'react';
 
 function App() {
+  const CLIENT_ID=process.env.SPOTIFY_CLIENT_ID;
+  const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
+  useEffect(()=>{
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+    
+    if(!token && hash){
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }, 
+      params: {
+        q:searchKey,
+        type: "artist"
+      }
+    })
+    setArtists(data.artists.items)
+  }
+  
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div key={artist.id}>
+        {artist.images.length ? <img width={"50%"} src={artist.images[0].url} alt=""></img> : <div>no image</div>} 
+        <br/>
+        {artist.name}
+        {console.log(artist.name)}
+      </div>
+    ))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>SpotiFly</h1>
+       {!token ? 
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button onClick={logout}>Logout</button>
+       }
+       {token ? 
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"} >Search</button>
+        </form>
+        : <h2>Please login to Spotify</h2>
+        }
+
+      {renderArtists()}
       </header>
     </div>
   );
---
+++ src/Library.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'; 
+
+const Library = (json) => {
+    const [songs, setSongs] = useState([]);
+    const [albums, setAlbums] = useState([]);
+    const [artists, setArtists] = useState([]);
+}
+
+export default Library;
